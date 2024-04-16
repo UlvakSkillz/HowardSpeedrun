6bf0cd71f22ce263eda3d6e9ef9d8293c30a572d
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2024-04-16 12:30:33 -0700

    Initial commit

---
+++ HowardSpeedrun/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using HowardSpeedrun; // The namespace of your mod class
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+// ...
+[assembly: MelonInfo(typeof(main), "HowardSpeedrun", "1.0.0", "UlvakSkillz")]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HowardSpeedrun")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HowardSpeedrun")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("95dd4b44-114b-446b-b143-721c49169f52")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HowardSpeedrun/main.cs
@@ -0,0 +1,95 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using RUMBLE.Environment.Howard;
+using RumbleModdingAPI;
+using System.Diagnostics;
+using TMPro;
+using UnityEngine;
+
+namespace HowardSpeedrun
+{
+    public class main : MelonMod
+    {
+        [HarmonyPatch(typeof(Howard), "OnActivationLeverChanged")]
+        public static class Patch0
+        {
+            private static void Postfix(int step)
+            {
+                if (step == 0)
+                {
+                    main.howardActive = true;
+                    main.timer.Restart();
+                }
+                else if (step == 1)
+                {
+                    main.howardActive = false;
+                    main.timer.Stop();
+                    main.timerTimeComponent.text = timer.Elapsed.ToString();
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Howard), "DealDamage")]
+        public static class Patch1
+        {
+            private static void Postfix()
+            {
+                if (main.howard.currentHp == 0) { howard.OnActivationLeverChanged(1); }
+            }
+        }
+
+        private string currentScene = "Loader";
+        private bool sceneChanged = false;
+        private bool loaded = false;
+        private GameObject timerGameObject, timerTextStatic, timerTime;
+        private static TextMeshPro timerTextComponent, timerTimeComponent;
+        private static Howard howard;
+        private static bool howardActive = false;
+        private static Stopwatch timer = new Stopwatch();
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            currentScene = sceneName;
+            sceneChanged = true;
+            howardActive = false;
+        }
+
+        public override void OnFixedUpdate()
+        {
+            if (currentScene != "Gym") { return; }
+            if (sceneChanged)
+            {
+                try
+                {
+                    howard = Calls.GameObjects.Gym.Logic.HeinhouserProducts.HowardRoot.GetGameObject().GetComponent<Howard>();
+                    timerGameObject = new GameObject();
+                    timerGameObject.name = "HowardTimer";
+                    timerTextStatic = GameObject.Instantiate(Calls.GameObjects.Gym.Logic.HeinhouserProducts.Leaderboard.PlayerTags.HighscoreTag0.Nr.GetGameObject());
+                    timerTextStatic.transform.parent = timerGameObject.transform;
+                    timerTextStatic.name = "TimerText";
+                    timerTextComponent = timerTextStatic.GetComponent<TextMeshPro>();
+                    timerTextComponent.text = "Time: ";
+                    timerTextComponent.fontSize = 5f;
+                    timerTextComponent.color = new Color(0, 0, 0, 1);
+                    timerTextComponent.outlineColor = new Color(0, 0, 0, 0.5f);
+                    timerTextComponent.alignment = TextAlignmentOptions.Left;
+                    timerTextComponent.enableWordWrapping = false;
+                    timerTextComponent.outlineWidth = 0.1f;
+                    timerTime = GameObject.Instantiate(timerTextStatic);
+                    timerTime.transform.parent = timerGameObject.transform;
+                    timerTime.name = "TimerTime";
+                    timerTimeComponent = timerTime.GetComponent<TextMeshPro>();
+                    timerTimeComponent.text = timer.Elapsed.ToString();
+                    timerTextStatic.transform.position = new Vector3(-2f, 1f, 0);
+                    timerTime.transform.position = new Vector3(-1f, 1f, 0);
+                    timerGameObject.transform.position = new Vector3(10.5091f, -0.9382f, -22.3673f);
+                    timerGameObject.transform.rotation = Quaternion.Euler(0, 133.5728f, 0);
+                    loaded = true;
+                    sceneChanged = false;
+                }
+                catch { return; }
+            }
+            if (loaded && howardActive) { timerTimeComponent.text = timer.Elapsed.ToString(); }
+        }
+    }
+}
